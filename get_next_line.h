631e9130bb3d1b3dd250d44fad0fffb48a955e7b
gguedes <gguedes@student.42.rio>
2022-05-28 16:01:40 -0300

    g_str not going to next line position

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,23 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:10:16 by gguedes           #+#    #+#             */
-/*   Updated: 2022/05/28 12:09:12 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/05/28 15:51:16 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 10
-#include <stdlib.h>
-#include <unistd.h>
+# define BUFFER_SIZE 42
+# include <stdlib.h>
+# include <unistd.h>
+
+static char	*g_str;
 
 char	*get_next_line(int fd);
+char	*ft_strjoin(char *s, char *tmp);
+char	*create_line(char *s);
+size_t	ft_strlen(char *s);
+int		new_line(char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,21 +6,94 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:53:09 by gguedes           #+#    #+#             */
-/*   Updated: 2022/05/27 10:55:29 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/05/28 15:59:30 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int	check_line(char	*str)
+//strlen
+size_t	ft_strlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+//verifica se existe uma quebra de linha na string
+int	new_line(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] != '\n')
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
 		i++;
-	if (str[i] == '\n')
-		return (i);
-	else
-		return (NULL);
+	}
+	return (0);
+}
+
+//cria a string que sera retornada
+char	*create_line(char *s)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+
+	i = 0;
+	while (s[i] != '\n')
+		i++;
+	i++;
+	new_str = (char *)malloc(i + 1 * sizeof(char));
+	if (!new_str)
+		return (0);
+	j = 0;
+	while (j < i)
+	{
+		new_str[j] = s[j];
+		j++;
+	}
+	printf("\n\nstr: \"%s\"\n\n", g_str);
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+//cria uma nova string, que é a soma das duas strings passadas
+char	*ft_strjoin(char *s, char *tmp)
+{
+	int		i;
+	char	*new_str;
+
+	new_str = (char *)malloc(ft_strlen(s) + ft_strlen(tmp) + 1 * sizeof(char));
+	if (!new_str)
+		return (0);
+	i = 0;
+	if (s)
+	{
+		while (*s)
+		{
+			new_str[i++] = *s;
+			s++;
+		}
+	}
+	if (tmp)
+	{
+		while (*tmp)
+		{
+			new_str[i++] = *tmp;
+			tmp++;
+		}
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
