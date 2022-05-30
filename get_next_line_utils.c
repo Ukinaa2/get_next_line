6afc32392e73fdd30e673ded3655737645f3ca0c
gguedes <gguedes@student.42.rio>
2022-05-30 14:09:56 -0300

    get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:53:09 by gguedes           #+#    #+#             */
-/*   Updated: 2022/05/30 13:58:34 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/05/30 14:08:59 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,44 @@ char	*ft_strjoin(char *s1, char *s2)
 	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1 * sizeof(char));
 	if (!new_str)
 		return (NULL);
+	i = 0;
 	while (*s1)
-	{
-		new_str[i++] = *s1;
-		s1++;
-	}
+		new_str[i++] = *s1++;
 	while (*s2)
+		new_str[i++] = *s2++;
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	char	*new_str;
+
+	new_str = (char *)malloc(ft_strlen(str) + 1 * sizeof(char));
+	if (!new_str)
+		return (0);
+	i = 0;
+	while (str[i])
 	{
-		new_str[i++] = *s2;
-		s2++;
+		new_str[i] = str[i];
+		i++;
 	}
 	new_str[i] = '\0';
-
 	return (new_str);
 }
 
 int	main(void)
 {
 	int		i;
-	char	*str;
+	char	*str1;
+	char	*str2;
 
-	str = ft_strjoin("a", "abcd");
-	printf("%s\n", str);
-	i = ft_strlen(str);
+	str1 = ft_strjoin("abcd", "efgh");
+	printf("%s\n", str1);
+	i = ft_strlen(str1);
 	printf("%i\n", i);
+	str2 = ft_strdup(str1);
+	printf("%s\n", str2);
 	return (0);
 }
