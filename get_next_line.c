9f7ee3c03d4f2fb89ca739898aa27efcebd9dbc4
gguedes <gguedes@student.42.rio>
2022-06-02 22:52:05 -0300

    Passing all tests get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,46 +6,93 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:05:57 by gguedes           #+#    #+#             */
-/*   Updated: 2022/05/31 18:15:20 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/06/02 19:39:05 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*g_str;
+char	*create_line(const char *str)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	new_str = (char *)malloc(i + 1 * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		new_str[j] = str[j];
+		j++;
+	}
+	new_str[j] = '\0';
+	return (new_str);
+}
+
+char	*find_next_line(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (str + (i + 1));
+		i++;
+	}
+	return (NULL);
+}
+
+char	*check_new_line(int fd, char *str, char *buffer)
+{
+	int			bytes_read;
+	char		*tmp;
+
+	bytes_read = 1;
+	while (!(find_next_line(str)) && bytes_read)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (NULL);
+		buffer[bytes_read] = '\0';
+		if (!str)
+			str = ft_strdup(buffer);
+		else
+		{
+			tmp = ft_strjoin(str, buffer);
+			free(str);
+			str = tmp;
+		}
+	}
+	return (str);
+}
 
 char	*get_next_line(int fd)
 {
-	int		bytes_read;
-	char	*buffer;
-	char	*new_str;
-	char	*tmp;
+	static char	*str;
+	char		*tmp;
+	char		*buffer;
+	char		*new_str;
 
-	if (fd < 0 || !BUFFER_SIZE)
-		return (NULL);
-	buffer = (char *)malloc(BUFFER_SIZE + 1 * sizeof(char));
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	while (!(ft_strchr(g_str, '\n')))
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (!bytes_read || bytes_read == -1)
-		{
-			free(buffer);
-			new_str = g_str;
-			g_str = NULL;
-			return (new_str);
-		}
-		buffer[bytes_read] = '\0';
-		tmp = ft_strjoin(g_str, buffer);
-		if (g_str)
-			free(g_str);
-		g_str = tmp;
-	}
+	str = check_new_line(fd, str, buffer);
 	free(buffer);
-	new_str = create_new_line(g_str);
-	tmp = ft_strdup(ft_strchr(g_str, '\n') + 1);
-	free(g_str);
-	g_str = tmp;
+	new_str = create_line(str);
+	tmp = ft_strdup((find_next_line(str)));
+	free(str);
+	str = tmp;
 	return (new_str);
 }
